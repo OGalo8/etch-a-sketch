cf2d24f1047e56fc6cc02340e882e1b76ff212cb
ogalo8 <arielgalo39481@gmail.com>
2025-02-16 17:03:32 -0600

    Adding the restart color functionality.

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # etch-a-sketch
+The project consists in creating an interactive grid that has a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
+
+The grid has been created and the coloring happens on-hover. 
+
+Now I want to create a color restart button, and add a color selector and a grid-size interactive bar.
+To restart, add an event listener to the restart button that sets the color of the container (or each .col element) to the original one.
+
+After the beforementioned is done, I will add a randomizer for the color, and a progressive darkening effect where each interaction darkens the square by 10%. The goal is to achieve a fully black (or completely colored) square in only ten interactions. (using the opacity property in CSS).
--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 
 //We will add a UI element so the user can enter the gridSize 
-gridSize = 16;
+let gridSize = 16;
+const container = document.querySelector(".grid-container");
 
-const container = document.querySelector(".container");
 
-function createRows(){
-    for(let i = 1; i<= gridSize; i++){
+
+// showing the grid size on top of the slider
+let slider = document.querySelector("#grid-size-slider");
+let output = document.querySelector("#grid-size-number");
+
+//functions to create the grid
+function createRows(rowsToCreate){
+    for(let i = 1; i<= rowsToCreate; i++){
         const newRow = document.createElement("div");
         newRow.setAttribute("class",`row`);
         newRow.setAttribute("id",`row-${i}`);
@@ -14,12 +20,12 @@ function createRows(){
             border: 1px solid grey;
             flex: auto;`);        
         container.appendChild(newRow);    
-        createColumns(newRow,i);
+        createColumns(newRow,i,rowsToCreate);
     }
 }
 
-function createColumns(newRow,i){
-    for (let j = 1; j<=gridSize; j++){
+function createColumns(newRow,i,rowsToCreate){
+    for (let j = 1; j<=rowsToCreate; j++){
         const newCol = document.createElement("div");
         newCol.setAttribute("class","col");
         newCol.setAttribute("id",`col-${i}-${j}`);
@@ -30,12 +36,54 @@ function createColumns(newRow,i){
     }
 }
 
-createRows();
+// creating an initial grid
+createRows(gridSize);
+
+// const allRows = document.querySelectorAll(".row");
+// for (let i = 0; i <= 2*gridSize; i++){
+//     allRows.item(i).remove();
+// }
+
+
+
+// remove grid when the slider moves, so it can create a new one
+// function removeRows(rowsToRemove){
+//     const allRows = document.querySelectorAll(".row");
+//     for (let i = 0; i <= rowsToRemove; i++){
+//         allRows.item(i).remove();
+//     }
+// }
+
+//updates the value of gridSize based on the slider's value
+// output.innerHTML = slider.value;
+// slider.oninput = function newGridSize(){
+//     //slider called the function, so 'this' knows that it must refer to slider.
+//     // it doesn't work the same way with arrow functions.
+//     output.innerHTML = this.value
+    
+//     let sliderGridSize = this.value;
+//     let difference = sliderGridSize - gridSize;
+//     if(difference > 0) createRows(difference);
+//     if(difference <0) removeRows(difference);
+// }
+
 
 // logic for changing color when mouse hovers over an element
-
-// const col11 = document.querySelector("#col-1-1")
 container.addEventListener("mouseover",(e)=> {
     if(e.target.className != "col") return;
     e.target.style.backgroundColor = "red";
-})
+});
+
+// logic for the restart button. It will eliminate the color of the grid.
+const restartBtn = document.querySelector("#restart-button");
+
+restartBtn.addEventListener("click",function decolor(e) {
+    if(!e.target.closest ("button")) return; //if we do not click exactly on the restart button, nothing will happen
+    const allCols = document.querySelectorAll(".col");
+    for (const currentCol of allCols){ //this is how we iterate through a nodeList
+        currentCol.style.backgroundColor = "darkgrey";
+    }
+});
+
+
+
