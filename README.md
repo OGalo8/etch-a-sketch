579d1131b07e65b12745870f5f2c23485d11f071
ogalo8 <arielgalo39481@gmail.com>
2025-02-17 22:39:02 -0600

    Added an 'increase opacity' button.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # etch-a-sketch
+
 The project consists in creating an interactive grid that has a “hover” effect so that the grid divs change color when your mouse passes over them, leaving a (pixelated) trail through your grid like a pen would.
 
-The grid has been created and the coloring happens on-hover. 
+The grid has been created and the coloring happens on-hover.
 
 Now I want to create a color restart button, and add a color selector and a grid-size (input range) interactive bar.
 To restart, add an event listener to the restart button that sets the color of the container (or each .col element) to the original one.
@@ -9,7 +10,17 @@ To restart, add an event listener to the restart button that sets the color of t
 After the beforementioned is done, I will add a randomizer for the color, and a progressive darkening effect where each interaction darkens the square by 10%. The goal is to achieve a fully black (or completely colored) square in only ten interactions. (using the opacity property in CSS).
 
 <!--  -->
+
 Added some styles (4 colour palette).
 The restart button and grid-size input range have been created and are functional.
 
 Will now add a color selector, random color button, random opacity button and erase function.
+
+<!--  -->
+
+Added an eraser and paint buttons. Also, added a color selector and a color randomizer button.
+Will now add a function to modify the opacity of the painting and, finally, will make the whole page flexible.
+
+<!--  -->
+
+The button to increase the opacity has been added. Now we only need to make the whole UI flexible as we shrink or grow the window, and then rewrite some parts of the code to avoid repetition. Functionality with window at full size is completed.
--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-
-//We will add a UI element so the user can enter the gridSize 
+//We will add a UI element so the user can enter the gridSize
 let gridSize = 16;
 const container = document.querySelector(".grid-container");
 
-
 //functions to create the grid
-function createGrid(rowsToCreate){
-    for(let i = 1; i<= rowsToCreate; i++){
-        const newRow = document.createElement("div");
-        newRow.setAttribute("class",`row`);
-        newRow.setAttribute("style",`
+function createGrid(rowsToCreate) {
+  for (let i = 1; i <= rowsToCreate; i++) {
+    const newRow = document.createElement("div");
+    newRow.setAttribute("class", `row`);
+    newRow.setAttribute(
+      "style",
+      `
             display: flex;
             border: 1px solid #B399D4;
-            flex: auto;`);        
-        container.appendChild(newRow);    
-        createColumns(newRow,i,rowsToCreate);
-    }
+            flex: auto;`
+    );
+    container.appendChild(newRow);
+    createColumns(newRow, i, rowsToCreate);
+  }
 }
 
-function createColumns(newRow,i,rowsToCreate){
-    for (let j = 1; j<=rowsToCreate; j++){
-        const newCol = document.createElement("div");
-        newCol.setAttribute("class","col");
-        newCol.setAttribute("style",`
+function createColumns(newRow, i, rowsToCreate) {
+  for (let j = 1; j <= rowsToCreate; j++) {
+    const newCol = document.createElement("div");
+    newCol.setAttribute("class", "col");
+    newCol.setAttribute(
+      "style",
+      `
             border: 1px solid #B399D4;
-            flex: auto;`);
-        newRow.appendChild(newCol);
-    }
+            flex: auto;`
+    );
+    newRow.appendChild(newCol);
+  }
 }
 
 // creating an initial grid
 createGrid(gridSize);
 
-
 // remove grid when the slider moves, so it can create a new one
-function removeGrid(){
-    const allRows = document.querySelectorAll(".row");
-    for (currentRow of allRows){
-        currentRow.remove();
-    }
+function removeGrid() {
+  const allRows = document.querySelectorAll(".row");
+  for (currentRow of allRows) {
+    currentRow.remove();
+  }
 }
 
 // showing the grid size on top of the slider
@@ -46,60 +49,85 @@ let slider = document.querySelector("#grid-size-slider");
 let output = document.querySelector("#grid-size-number");
 
 //updates the value of the grid based on the slider's value
-output.textContent = `${slider.value}x${slider.value}`
-slider.addEventListener("input",(e) => { 
-    output.textContent = `${e.target.value}x${e.target.value}`
-    removeGrid();
-    gridSize = e.target.value;
-    createGrid(gridSize);
-})
-
+output.textContent = `${slider.value}x${slider.value}`;
+slider.addEventListener("input", (e) => {
+  output.textContent = `${e.target.value}x${e.target.value}`;
+  removeGrid();
+  gridSize = e.target.value;
+  createGrid(gridSize);
+});
 
 // changing color when mouse hovers over an element
-function changeGridColor(color){
-    container.addEventListener("mouseover",(e)=> {
-        if(e.target.className != "col") return;
-        e.target.style.backgroundColor = color;
-    });
+function changeGridColor(color) {
+  container.addEventListener("mouseover", (e) => {
+    if (e.target.className != "col") return;
+    e.target.style.backgroundColor = color;
+  });
 }
 
 changeGridColor("#3E2A47");
 
 // color picker changes the color of the grid based on selection
 const colorPicker = document.querySelector("#color-picker");
-colorPicker.addEventListener("input", (e)=>{
-    changeGridColor(e.target.value);
-})
+colorPicker.addEventListener("input", (e) => {
+  changeGridColor(e.target.value);
+});
 
 // Remove draw button will eliminate the color of the grid.
 const resetBtn = document.querySelector("#reset-button");
 
-resetBtn.addEventListener("click",function decolor(e) {
-    if(!e.target.closest ("button")) return; //if we do not click exactly on the restart button, nothing will happen
-    const allCols = document.querySelectorAll(".col");
-    for (const currentCol of allCols){ //this is how we iterate through a nodeList
-        currentCol.style.backgroundColor = "#F5E1FD";
-    }
-    colorPicker.value = "#3E2A47"
-    changeGridColor("#3E2A47");
+resetBtn.addEventListener("click", function decolor(e) {
+  if (!e.target.closest("button")) return; //if we do not click exactly on the restart button, nothing will happen
+  const allCols = document.querySelectorAll(".col");
+  for (const currentCol of allCols) {
+    //this is how we iterate through a nodeList
+    currentCol.style.backgroundColor = "#F5E1FD";
+  }
+  colorPicker.value = "#3E2A47";
+  changeGridColor("#3E2A47");
 });
 
-// Adding an "erase" button that will change the colored squares in the grid back to their initial color 
+// Adding an "erase" button that will change the colored squares in the grid back to their initial color
 const eraserBtn = document.querySelector("#erase-button");
-eraserBtn.addEventListener("click",(e)=>{
-    changeGridColor("#F5E1FD")
-})
+eraserBtn.addEventListener("click", (e) => {
+  changeGridColor("#F5E1FD");
+});
 
 const paintBtn = document.querySelector("#paint-button");
-paintBtn.addEventListener("click",(e)=> {    
-    changeGridColor("#3E2A47");
+paintBtn.addEventListener("click", (e) => {
+  changeGridColor("#3E2A47");
 });
 
 // we will now add a button that will randomize colors as we paint in our grid
-function getRandomInt(max = 255) {
-    return Math.floor(Math.random() * max);
-  }
-
-function generateRandomColor(){
-    console.log( `rgb(${getRandomInt()},${getRandomInt()},${getRandomInt()})`);
+function getRandomInt(min = 0, max = 256) {
+  const minCeiled = Math.ceil(min);
+  const maxFloored = Math.floor(max);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
+
+function generateRandomColor(a) {
+  //will return a random color in rgb format
+  let selectedColor = `rgba(${getRandomInt()},${getRandomInt()},${getRandomInt()},${a})`;
+  return selectedColor;
+}
+
+const randomBtn = document.querySelector("#random-button");
+randomBtn.addEventListener("click", (e) => {
+  container.addEventListener("mouseover", (e) => {
+    if (e.target.className != "col") return;
+    let opacity = Number(Math.random().toFixed(2));
+    e.target.style.backgroundColor = generateRandomColor(opacity); //we generate the color just when the event of entering a new "col" happens
+  });
+});
+
+//we will now add an effect to increase opacity so that, in a max of 10 interactions, we have a fully black color
+const opacityBtn = document.querySelector("#opacity-button");
+
+opacityBtn.addEventListener("click", (e) => {
+  let opacity = 0;
+  container.addEventListener("mouseover", (e) => {
+    if (e.target.className != "col") return;
+    opacity += 0.1;
+    e.target.style.backgroundColor = `rgba(0,0,0,${opacity})`;
+  });
+});
